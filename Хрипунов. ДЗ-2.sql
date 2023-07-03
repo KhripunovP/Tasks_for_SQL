b62403ead92a3926aa05f7d63b1e28d33530b29d
Khripunov_P <dante-pash@mail.ru>
2023-07-03 22:45:02 +0300

    Task2 completed

---
+++ Хрипунов. ДЗ-2.sql
@@ -0,0 +1,47 @@
+DROP DATABASE IF EXISTS lesson2;
+CREATE DATABASE IF NOT EXISTS lesson2;
+
+USE lesson2;
+CREATE TABLE sales
+( id INT PRIMARY KEY AUTO_INCREMENT,
+order_date DATE NOT NULL,
+count_product INT);
+
+INSERT INTO sales (order_date, count_product)
+VALUES
+("2022-01-01", 156),
+("2022-01-02", 180),
+("2022-01-03", 21),
+("2022-01-04", 124),
+("2022-01-05", 341);
+
+SELECT id,
+CASE 
+WHEN count_product < 100 THEN "Маленький заказ"
+WHEN count_product > 300 THEN "Большой заказ"
+ELSE "Средний заказ"
+END AS message
+FROM sales;
+
+CREATE TABLE orders
+(id INT PRIMARY KEY AUTO_INCREMENT,
+employee_id VARCHAR(10),
+amount INT,
+order_status VARCHAR(45));
+
+INSERT INTO orders (employee_id, amount, order_status)
+VALUES
+("e03", 15.00, "OPEN"),
+("e01", 25.50, "OPEN"),
+("e05", 100.70, "CLOSED"),
+("e02", 22.18, "OPEN"),
+("e04", 9.50, "CANCELLED");
+
+SELECT *,
+IF (order_status = "OPEN", "Order is in open state", 
+IF (order_status = "CLOSED", "Order is in closed", "Order is cancelled"))
+AS full_order_status
+FROM orders
+
+
+
