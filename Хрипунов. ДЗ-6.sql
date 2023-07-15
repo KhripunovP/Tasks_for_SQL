16a4c387d539749f40061b7cd9bd7efe36ca2b96
Khripunov_P <dante-pash@mail.ru>
2023-07-15 17:54:24 +0300

    Homework 6

--- Хрипунов. ДЗ-6.sql
+++ Хрипунов. ДЗ-6.sql
@@ -6,25 +6,6 @@ CREATE DATABASE IF NOT EXISTS lesson6;
 
 USE lesson6;
 
-delimiter $$a
-CREATE FUNCTION fun1(num INT)
-RETURNS VARCHAR(100)  
-DETERMINISTIC
-BEGIN
-DECLARE days INT ;
-DECLARE hours INT;
-DECLARE minutes INT;
-DECLARE seconds INT;
-SET days = num / 86400;
-SET hours = num % 86400 / 3600;
-SET minutes = num % 86400 % 3600 / 60;
-SET seconds = num % 60;
-RETURN CONCAT(days, " days ", hours, " hours ", minutes, " minutes ", seconds, " seconds!");
-END $$a
-delimiter ;
-
-SELECT fun1(123456);
-
 delimiter &&
 CREATE PROCEDURE proc1(num INT)
 BEGIN
@@ -40,4 +21,27 @@ BEGIN
 END &&
 delimiter ;
 
-CALL proc1(1);
+CALL proc1(123456);
+
+-- Создайте функцию, которая  выводит только четные числа от 1 до 10 включительно.
+-- Пример: 2,4,6,8,10 (можно сделать через шаг +  2: х = 2, х+=2)
+
+delimiter $$a
+CREATE FUNCTION fun1(num INT)
+RETURNS VARCHAR(50)
+DETERMINISTIC
+BEGIN
+DECLARE result VARCHAR(50) DEFAULT 0;
+	WHILE num <= 10 DO
+		IF num % 2 = 0 THEN
+		SET result = CONCAT(result, ' ', num);
+		END IF;
+	SET num = num + 1;
+    END WHILE;
+RETURN result;
+END $$a
+
+delimiter ;
+
+SELECT fun1(1);
+
